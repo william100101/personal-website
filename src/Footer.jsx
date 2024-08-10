b204d4855722b98793fc5b6d369b58ac4a9e00f6
william100101 <wcc007@bucknell.edu>
2024-08-10 12:33:29 -0400

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+function Footer() {
+    return(
+        <h1>
+            
+        </h1>
+    )
+}
+
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+function Header() {
+    return(
+        <h1>
+            
+        </h1>
+    )
+}
+
+export default Header;
